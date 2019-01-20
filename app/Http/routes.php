75428ae568b8be7bb164692257b536faa116e6b9
ztray1 <zhangtuo332@gmail.com>
2019-01-20 12:24:35 -0500

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,7 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +19,9 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/admin',function(){
+    return view('admin.index');
+});
+
+Route::resource('admin/users','AdminUsersController');
